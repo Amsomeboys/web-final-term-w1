50abae280534a2f5dbd5d2db9c5d53ffa2f2b2a4
Amsomeboys <khnun1090@gmail.com>
2024-01-22 18:44:54 +0700

    fix form bug

--- myForm/script.js
+++ myForm/script.js
@@ -5,18 +5,27 @@ const password = document.getElementById("password");
 const confirm = document.getElementById("confirm");
 
 const checkPass = (password, confirm) => {
-	password.value === confirm.value
-		? form.reset()
-		: showErrorMessage("รหัสผ่านไม่ตรงกัน", confirm);
+	password.value !== ""
+		? password.value === confirm.value
+			? (hideErrorMessage(confirm), hideErrorMessage(password))
+			: showErrorMessage("รหัสผ่านไม่ตรงกัน", confirm)
+		: showErrorMessage("กรุณากรอกรหัสผ่าน", password);
 };
 
 const checkUsername = (min, max) => {
-	min > username.value
+	min > username.value.length
 		? showErrorMessage("ชื่อผู้ใช้ต้องมากกว่า 6", username)
 		: max < username.value.length
 		? showErrorMessage("ชื่อผู้ใช้ต้องน้อยกว่า 10", username)
-		: form.reset(),
-		(username.parentElement.querySelector("small").className = "frmDiv");
+		: min < username.value.length <= max
+		? hideErrorMessage(username)
+		: "";
+};
+
+const checkEmail = (action) => {
+	action.action.value !== ""
+		? hideErrorMessage(action)
+		: showErrorMessage("", action);
 };
 
 const showErrorMessage = (message, action) => {
@@ -25,10 +34,14 @@ const showErrorMessage = (message, action) => {
 	formControl.className = "frmDiv error";
 	small.innerText = message;
 };
+const hideErrorMessage = (action) => {
+	const formControl = action.parentElement;
+	formControl.className = "frmDiv pass";
+};
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	checkPass(password, confirm);
 	checkUsername(6, 10);
-	console.log(username.value);
+	checkEmail(email);
 });
